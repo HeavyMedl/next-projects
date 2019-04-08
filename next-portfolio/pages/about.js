ee941138d1cf75f9a3618e1c36febd475d8db49b
Kurt Medley <kurtdmedley@gmail.com>
2019-04-07 17:52:54 -0700

    Initial commit for next-projects

---
+++ next-portfolio/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout title="About">
+    <h1>About</h1>
+    <p>A JavaScript programmer</p>
+    <img src="/static/JavaScript-logo.png" alt="js-logo" height="200" />
+  </Layout>
+);
---
+++ next-portfolio/pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Index = () => (
+  <Layout title="Home">
+    <h1>Home</h1>
+    <Link href="/about">
+      <a>Go to about</a>
+    </Link>
+    <p>Weclome to the home page</p>
+  </Layout>
+);
+
+export default Index;
